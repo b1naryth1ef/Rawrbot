bd0c71c7a0c285be233d782e30c6ee5f1375c0b7
Andrei Z <b1naryth1ef@gmail.com>
2012-10-22 01:42:59 -0500

    Run the updater plz

--- updater/app.py
+++ updater/app.py
@@ -7,3 +7,6 @@ red = redis.Redis(host='hydr0.com')
 def hook_view():
     print request.json
     red.publish('irc.m', json.dumps({'tag':'UPD'}))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9010)
