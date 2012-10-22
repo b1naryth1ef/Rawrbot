db9d1f7177c36bb5069a002a3213ff2a37994227
Andrei Z <b1naryth1ef@gmail.com>
2012-10-22 01:52:06 -0500

    Remove test

--- master/main.py
+++ master/main.py
@@ -277,7 +277,7 @@ class Master(object):
         s.unsubscribe('irc.m')
 
     def masterLoop(self):
-        print 'Startin master loop! (test)'
+        print 'Startin master loop!'
         self.sub = red.pubsub()
         self.sub.subscribe('irc.master')
         while self.isMaster and self.active:
