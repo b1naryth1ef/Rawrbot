afe436220b7e9318ee02bdfda2ad16a8c1d17666
B1naryTh1ef <b1naryth1ef@gmail.com>
2012-12-03 15:40:15 -0600

    Final tweak

--- master/api.py
+++ master/api.py
@@ -173,7 +173,7 @@ class API(object):
             if not self.red.exists('i.%s.user.%s.whoisd' % (data['nid'], data['nick'].lower())):
                 m = {'tag': 'WHOIS', 'nick': data['nick']}
                 self.red.rpush('i.%s.worker.%s' % (data['nid'], data['id']), json.dumps(m))
-                time.sleep(5)
+                time.sleep(2)
         v = self.red.get('i.%s.user.%s.auth' % (data['nid'], data['nick'].lower()))
         if v: v = v.lower()
         b = self.red.sismember('i.%s.admins' % (data['nid']), v)
