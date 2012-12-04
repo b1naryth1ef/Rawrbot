051f79c06a3e0b743a19073ce9d6800929c01183
Andrei Z <b1naryth1ef@gmail.com>
2012-12-04 00:57:32 -0600

    UrTTv fixes

--- master/plugins/urttv.py
+++ master/plugins/urttv.py
@@ -11,21 +11,25 @@ def addGtvServer(kwargs):
 def getGtvServers():
     servers = []
     for key in A.red.keys('i.p.urttv.server.*'):
-        servers.append(A.red.hget(key))
+        servers.append(A.red.hgetall(key))
 
 #!gtv
 #!gtv addserver ip=blah.blah.com pw=joinpw cam=campw admin=adminpw, host=blah
 @P.cmd('.gtv', admin=True, usage='{cmd} <cmd> [args]', kwargs=True) #the . is for compatability right now
 def cmdGTV(obj):
     _usage = "!gtv addserver ip=127.0.0.1 cam=CAMPASS admin=ADMINPASS (not required:) pw=SERVERPASS host=HOSTERNAME"
-    if len(obj.m) < 2: obj.usage()
+    if len(obj.m) < 2: return obj.usage()
     if obj.m[1] not in GTV_COMMANDS:
         return obj.reply('Unknown GTV command "%s"' % obj.m[1])
     if obj.m[1] == 'servers':
-        obj.reply('GTV Servers: ')
-        for inc, i in enumerate(getGtvServers()):
-            i['id'] = inc+1
-            "#{id}: {ip} PW: {pw} Camera: {cam} Admin: {admin} Hoster: {host}".format(**i)
+        x = getGtvServers()
+        if len(x):
+            obj.reply('GTV Servers: ')
+            for inc, i in enumerate(x):
+                i['id'] = inc+1
+                "#{id}: {ip} PW: {pw} Camera: {cam} Admin: {admin} Hoster: {host}".format(**i)
+        else:
+            return obj.reply('No GTV servers!')
     elif obj.m[1] == 'rmvserver':
         id = obj.kwargs.get('id')
         if not id and len(obj.m) > 2: id = obj.m[2]
@@ -36,7 +40,7 @@ def cmdGTV(obj):
             return obj.reply('Deleted server #%s successfully!' % id)
         else: return obj.reply('No server with ID #%s!' % id)
     elif obj.m[1] == 'addserver':
-        if not obj.kwargs.get('ip') and obj.kwargs.get('cam') and obj.kwargs.get('admin'):
+        if not obj.kwargs.get('ip') or not obj.kwargs.get('cam') or not obj.kwargs.get('admin'):
             return obj.reply("Usage: %s" % _usage)
         val = {}
         val['ip'] = obj.kwargs.get('ip')
